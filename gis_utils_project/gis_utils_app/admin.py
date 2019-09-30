ebe2de78e9219466c0b833e5ce021509e00212ed
tana1024 <keiskbusiness@gmail.com>
2019-09-30 09:04:02 +0000

    admin view table

--- gis_utils_project/gis_utils_app/admin.py
+++ gis_utils_project/gis_utils_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import ClientUpdateStatus, Client
+
 # Register your models here.
+admin.site.register(ClientUpdateStatus)
+admin.site.register(Client)
