2340b26f080c2e3f720cbf98edbd93a2ed073c5c
tana1024 <52588824+tana1024@users.noreply.github.com>
2019-10-22 20:18:25 +0900

    Update settings.py

--- gis_utils_project/gis_utils_project/settings.py
+++ gis_utils_project/gis_utils_project/settings.py
@@ -167,8 +167,8 @@ except ImportError:
 
 if not DEBUG:
     SECRET_KEY = os.environ['SECRET_KEY']
-    import django_heroku
-    django_heroku.settings(locals())
+#    import django_heroku
+#    django_heroku.settings(locals())
 
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
