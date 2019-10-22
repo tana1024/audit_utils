886e38914e945d069f8a381c3cec1bd4697b1f9a
tana1024 <52588824+tana1024@users.noreply.github.com>
2019-10-22 19:59:41 +0900

    Update local_settings.py

--- gis_utils_project/gis_utils_project/local_settings.py
+++ gis_utils_project/gis_utils_project/local_settings.py
@@ -1,6 +1,6 @@
 import os
 
-#SECRET_KEY = 'ad($!z@i^9$ai@gd!6j4f!7fobv#f8pq%ux44_b=695kd1vf(z' # 追加
+SECRET_KEY = 'ad($!z@i^9$ai@gd!6j4f!7fobv#f8pq%ux44_b=695kd1vf(z' # 追加
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
