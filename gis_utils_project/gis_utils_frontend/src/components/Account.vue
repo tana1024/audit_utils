cf78fe288a22f8f1a2da153d50ee74347c67f702
tana1024 <keiskbusiness@gmail.com>
2019-11-10 09:50:06 +0000

    account implementation

--- gis_utils_project/gis_utils_frontend/src/components/Account.vue
+++ gis_utils_project/gis_utils_frontend/src/components/Account.vue
@@ -5,42 +5,47 @@
         Create Account
       </div>
       <div class="col-8 mt-5">
-        <div class="mb-2">
-          <label for="username">Username</label>
-        </div>
-        <div class="mb-3">
-          <validation-provider v-slot="{ errors }" rules="required|alpha_dash|max:50" name="Username">
-            <b-form-input type="text" id="username" v-model="userName" class="w-50"></b-form-input>
-            <p v-show="errors.length" class="alert alert-danger w-50">
-              {{ errors[0] }}
-            </p>
-          </validation-provider>
-        </div>
-        <div class="mb-2">
-          <label for="password">Password</label>
-        </div>
-        <div class="mb-3">
-          <validation-provider v-slot="{ errors }" rules="required|alpha_dash|max:50" name="Password">
-            <b-form-input type="password" id="password" v-model="password" placeholder="enter a password" class="w-50" v-b-tooltip.hover title="Password must be 8 or more characters."></b-form-input>
-            <p v-show="errors.length" class="alert alert-danger w-50">
-              {{ errors[0] }}
-            </p>
-          </validation-provider>
-        </div>
-        <div class="mb-2">
-          <label for="c-password">Confirm Password</label>
-        </div>
-        <div class="mb-4">
-          <validation-provider v-slot="{ errors }" rules="required|alpha_dash|max:50" name="Confirm Password">
-            <b-form-input type="password" id="c-password" v-model="cPassword" placeholder="enter the password again" class="w-50"></b-form-input>
-            <p v-show="errors.length" class="alert alert-danger w-50">
-              {{ errors[0] }}
-            </p>
-          </validation-provider>
-        </div>
-        <div class="mb-3">
-          <b-button id="ca-button" variant="primary">Create Account</b-button>
-        </div>
+        <validation-observer v-slot="{ invalid }">
+          <div class="mb-2">
+            <label for="username">Username</label>
+          </div>
+          <div class="mb-3">
+            <validation-provider v-slot="{ errors }" rules="required|alpha_dash|max:50" name="Username">
+              <b-form-input type="text" id="username" v-model="userName" class="w-50"></b-form-input>
+              <p v-show="errors.length" class="alert alert-danger w-50">
+                {{ errors[0] }}
+              </p>
+            </validation-provider>
+          </div>
+          <div class="mb-2">
+            <label for="password">Password</label>
+          </div>
+          <div class="mb-3">
+            <validation-provider v-slot="{ errors }" rules="required|alpha_dash|max:50" name="Password">
+              <b-form-input type="password" id="password" v-model="password" placeholder="enter a password" class="w-50" v-b-tooltip.hover title="Password must be 8 or more characters."></b-form-input>
+              <p v-show="errors.length" class="alert alert-danger w-50">
+                {{ errors[0] }}
+              </p>
+            </validation-provider>
+          </div>
+          <div class="mb-2">
+            <label for="c-password">Confirm Password</label>
+          </div>
+          <div class="mb-4">
+            <validation-provider v-slot="{ errors }" rules="required|alpha_dash|max:50" name="Confirm Password">
+              <b-form-input type="password" id="c-password" v-model="cPassword" placeholder="enter the password again" class="w-50"></b-form-input>
+              <p v-show="errors.length" class="alert alert-danger w-50">
+                {{ errors[0] }}
+              </p>
+            </validation-provider>
+          </div>
+          <div class="mb-3">
+            <b-button id="ca-button" variant="primary" :disabled="invalid" @click="submit">Create Account</b-button>
+          </div>
+          <p v-show="invalid" class="alert alert-danger w-50">
+             入力項目が適切に入力されていません。
+          </p>
+        </validation-observer>
       </div>
     </div>
   </div>
@@ -61,6 +66,18 @@ export default {
   components: {
     ValidationProvider,
     ValidationObserver
+  },
+  data: function () {
+    return {
+      userName: '',
+      password: '',
+      cPassword: ''
+    }
+  },
+  methods: {
+    submit: function() {
+      alert('submit start')
+    }
   }
 }
 </script>
