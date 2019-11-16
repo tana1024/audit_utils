79f18b2aedfef14e07b408446fbe5dc871037a5e
tana1024 <keiskbusiness@gmail.com>
2019-11-16 08:30:38 +0000

    testcase implements

--- gis_utils_project/gis_utils_app/tests/test/views/test_information_views.py
+++ gis_utils_project/gis_utils_app/tests/test/views/test_information_views.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
 from django.urls import reverse
 from ....models import News
-from ...factory.NewsFactory import NewsFactory
+from ...factory.NewsFactory import NewsFactory, NewsAllFuzzyFactory
+import datetime
+import pytz
 
 # Create your tests here.
 class InitInformationViewTest(TestCase):
@@ -9,17 +11,40 @@ class InitInformationViewTest(TestCase):
 
     def setUp(self):
         print('setUp start')
+
+    def test_init_single(self):
+        print('test_init_single start')
         factory = NewsFactory
+        factory.create_batch(size=1)
+
+        url = reverse('init_information_view', urlconf='gis_utils_app.urls')
+        print(url)
+        response = self.client.get(self.URL_ROOT + url)
+        print(response.data)
+        self.assertEquals(response.status_code, 200)        
+        self.assertEquals(response.data[0]['source_name'], 'hogehoge_news')
+        self.assertEquals(response.data[0]['title'], 'comming alien')
+        self.assertEquals(response.data[0]['title_jp'], 'エイリアンが来た')
+        self.assertEquals(response.data[0]['description'], 'comming alien')
+        self.assertEquals(response.data[0]['description_jp'], 'エイリアンが来た')
+        self.assertEquals(response.data[0]['url'], 'https://hogehoge.hoge.hoge')
+        self.assertEquals(response.data[0]['published_at'], '2019-01-16T01:17:30+09:00')
+
+        News.objects.all().delete()
+
+    def test_init_multi(self):
+        print('test_init_multi start')
+        factory = NewsAllFuzzyFactory
         factory.create_batch(size=10)
 
-    def test_init(self):
-        print('test_init start')
         url = reverse('init_information_view', urlconf='gis_utils_app.urls')
         print(url)
         response = self.client.get(self.URL_ROOT + url)
         print(response.data)
         self.assertEquals(response.status_code, 200)
+        self.assertEquals(len(response.data), 10)
+
+        News.objects.all().delete()
 
     def tearDown(self):
         print('tearDown start')
-        News.objects.all().delete()
